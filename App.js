4369296f1f1ee2d5f1bed3d0a35d8aee4c9935d2
priyanga <mpriyanga1@gmail.com>
2019-04-14 11:08:48 +0800

    ..Initial Commit

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ import { Provider } from 'react-redux';
 import configureStore from './src/configureStore';
 import HomeScreen from './src/components/HomeScreen';
 import DetailScreen from './src/components/DetailScreen';
-import { createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 
 const store = configureStore()
 export default class App extends Component {
